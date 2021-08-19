fb66ffcbe1e98ea787c78bf56918b26316930efb
FredrikEglese <fredrik.eglese@gmail.com>
2021-08-19 19:06:53 +0100

    Sort server.js and use morgan for dev logging

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+// NPM Packages
 const express = require("express");
 const dotenv = require("dotenv");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const morgan = require("morgan");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// Mount routers
+// Mount middleware
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+// Mount routes
+const bootcamps = require("./routes/bootcamps");
 app.use("/api/v1/bootcamps", bootcamps);
 
-const PORT = process.env.PORT || 5000;
-
+// Start listening
 app.listen(
 	PORT,
 	console.log(`Process runnning in ${process.env.NODE_ENV} mode on ${PORT}`)
