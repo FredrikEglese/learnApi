4ed04666377ecda79e2a1ee8037d8cc42d4c06e3
FredrikEglese <fredrik.eglese@gmail.com>
2021-08-30 12:00:01 +0100

    Add mongo connection and colors

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
@@ -19,7 +24,18 @@ const bootcamps = require("./routes/bootcamps");
 app.use("/api/v1/bootcamps", bootcamps);
 
 // Start listening
-app.listen(
+const server = app.listen(
 	PORT,
-	console.log(`Process runnning in ${process.env.NODE_ENV} mode on ${PORT}`)
+	console.log(
+		`Process runnning in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+	)
 );
+
+// Handle Unhandled rejections
+
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`Error: ${err.message}`.red.bold);
+	server.close(() => {
+		process.exit(1);
+	});
+});
