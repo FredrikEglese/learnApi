168cd58872df9cffc480bc6712485f8791cdaeba
FredrikEglese <fredrik.eglese@gmail.com>
2021-08-31 22:16:32 +0100

    Seed data for bootcamps

---
+++ seeder.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Load models
+const Bootcamp = require("./models/Bootcamp");
+
+// Connect to db
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+// Import into db
+const importData = async () => {
+	try {
+		await Bootcamp.create(bootcamps);
+		console.log("Data imported...".green.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(error.message);
+	}
+};
+
+const deleteData = async () => {
+	try {
+		await Bootcamp.deleteMany();
+
+		console.log("Data destroyed...".red.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(error.message);
+	}
+};
+
+if (process.argv[2] === "-i") {
+	importData();
+} else if (process.argv[2] === "-d") {
+	deleteData();
+}
