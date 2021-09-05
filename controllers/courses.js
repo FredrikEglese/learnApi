b799c0bacc207717bce063d20cf0afecee630ef2
FredrikEglese <fredrik.eglese@gmail.com>
2021-09-05 19:40:15 +0100

    Add route for updating course details

--- controllers/courses.js
+++ controllers/courses.js
@@ -69,3 +69,26 @@ exports.addCourse = AsyncHandler(async (req, res, next) => {
 		data: newCourse,
 	});
 });
+
+// @desc 		Update a course
+// @route 	PUT /api/v1/courses/:id
+// @access 	Private
+exports.updateCourse = AsyncHandler(async (req, res, next) => {
+	let course = await Course.findById(req.params.id);
+
+	if (!course) {
+		return next(
+			new ErrorResponse(`No course found with id of ${req.params.id}`, 404)
+		);
+	}
+
+	course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(200).json({
+		success: true,
+		data: course,
+	});
+});
