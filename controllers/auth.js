84a8ef6def6f339105ae9531425374cb7486cdc3
FredrikEglese <fredrik.eglese@gmail.com>
2021-09-10 15:54:06 +0100

    Create a user with a hashed password

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,18 @@ const AsyncHandler = require("../middleware/async");
 // @route   POST /api/v1/user/
 // @access  Public
 exports.registerUser = AsyncHandler(async (req, res, next) => {
-	res.status(201).json({ success: true });
+	const { name, email, password, role } = req.body;
+
+	// Create user
+	const user = await User.create({
+		name,
+		email,
+		password,
+		role,
+	});
+
+	// Create token
+	const token = user.getSignedJwtToken();
+
+	res.status(201).json({ success: true, token });
 });
